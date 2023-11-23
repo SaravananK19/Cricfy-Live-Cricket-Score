d7356cf45819810f21b9ee783360147365a98744
SaravananK19 <149501627+SaravananK19@users.noreply.github.com>
2023-11-23 15:47:42 +0530

    Update Project.py

--- Cricfy/Project.py
+++ Cricfy/Project.py
@@ -5,7 +5,9 @@
 import logging
 import requests
 import time
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor, types #that represent different types of objects in the Telegram API.t
+#ypes.Message: Represents a message sent by a user.types.User: Represents a Telegram user.types.Chat: Represents a chat in Telegram
+types.Chat: Represents a chat in Telegram (which can be a private chat, group, or channel).
 #dispatcher-  used to handle updates and route them to the appropriate handlers. 
 #Handlers are used to define how your bot should respond to different types of user input or system events.
 
